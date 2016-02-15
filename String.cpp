8934ccefbc1cde567a4541f3d204a262b0369eaa
Antoine Weber <antoine.weber@insa-lyon.fr>
2016-02-15 17:01:56 +0100

    merge

--- String.cpp
+++ String.cpp
@@ -36,8 +36,8 @@ String::String(const String& str){
 
 String::String(const char* str){
   
-	size_ = str.size_ ;
-	capacity_ = str.capacity_;
+	size_ = (str).size_ ;
+	capacity_ = (str).capacity_;
 	tab_ = new char[capacity_];
 	for(int i = 0; i<size_; i++){
 		tab_[i] = str.tab_[i];
